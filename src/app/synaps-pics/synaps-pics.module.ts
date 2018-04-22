3b9c7e75b58719ace2ded8f8b2f8a5c873fee7b0
Murat Çorlu <muratcorlu@gmail.com>
2018-04-22 18:02:41 +0200

    Some extra exports

--- src/app/synaps-pics/synaps-pics.module.ts
+++ src/app/synaps-pics/synaps-pics.module.ts
@@ -5,13 +5,20 @@ import { SynapsPicsUtils } from './synaps-pics.utils';
 import { SynapsPicsConfig } from './synaps-pics.config';
 import { SynapsImageOptions } from './synaps-image-options.model';
 
+export { SynapsPicsUtils } from './synaps-pics.utils';
+export { SynapsPicsConfig } from './synaps-pics.config';
+export { SynapsImageOptions } from './synaps-image-options.model';
+
 @NgModule({
   declarations: [
     SynapsPicsDirective
   ],
   exports: [
     CommonModule,
-    SynapsPicsDirective
+    SynapsPicsDirective,
+    SynapsPicsUtils,
+    SynapsImageOptions,
+    SynapsPicsConfig
   ],
   providers: [
     SynapsPicsUtils,

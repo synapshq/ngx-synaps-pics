184eb2f099dc8bc5974cf245620328fa38633b5c
Murat Çorlu <muratcorlu@gmail.com>
2018-04-23 03:43:20 +0200

    Exports fixed

--- src/app/synaps-pics/synaps-pics.module.ts
+++ src/app/synaps-pics/synaps-pics.module.ts
@@ -5,20 +5,13 @@ import { SynapsPicsUtils } from './synaps-pics.utils';
 import { SynapsPicsConfig } from './synaps-pics.config';
 import { SynapsImageOptions } from './synaps-image-options.model';
 
-export { SynapsPicsUtils } from './synaps-pics.utils';
-export { SynapsPicsConfig } from './synaps-pics.config';
-export { SynapsImageOptions } from './synaps-image-options.model';
-
 @NgModule({
   declarations: [
     SynapsPicsDirective
   ],
   exports: [
     CommonModule,
-    SynapsPicsDirective,
-    SynapsPicsUtils,
-    SynapsImageOptions,
-    SynapsPicsConfig
+    SynapsPicsDirective
   ],
   providers: [
     SynapsPicsUtils,
--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,5 @@
+export * from './app/synaps-pics/synaps-image-options.model';
+export * from './app/synaps-pics/synaps-pics.config';
+export * from './app/synaps-pics/synaps-pics.utils';
+export * from './app/synaps-pics/synaps-pics.directive';
 export * from './app/synaps-pics/synaps-pics.module';
